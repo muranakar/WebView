02fe30587c97a73527a3ed638b2ea61e15666086
muranakar <P1211065@gmail.com>
2024-02-21 07:11:25 +0900

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useRef } from "react";
+import { View, StyleSheet, Button } from "react-native";
+import { WebView } from "react-native-webview";
 
-export default function App() {
+const MyWebView = () => {
+  const webViewRef = useRef(null);
+
+  const handleGoBack = () => {
+    if (webViewRef.current) {
+      webViewRef.current.goBack();
+    }
+  };
+
+  const handleReload = () => {
+    if (webViewRef.current) {
+      webViewRef.current.reload();
+    }
+  };
+
+  const handleGoToSpecificUrl = () => {
+    if (webViewRef.current) {
+      webViewRef.current.injectJavaScript(
+        `window.location.href = 'https://www.anime-chiikawa.jp/';`
+      );
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <WebView
+        ref={webViewRef}
+        source={{ uri: "https://www.google.com/" }}
+        style={styles.webview}
+      />
+      <View style={styles.buttonContainer}>
+        <Button title="戻る" onPress={handleGoBack} />
+        <Button title="リロード" onPress={handleReload} />
+        <Button title="特定のリンクへ遷移" onPress={handleGoToSpecificUrl} />
+      </View>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 40,
+    marginBottom: 40,
+  },
+  webview: {
+    flex: 1,
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+    padding: 10,
   },
 });
+
+export default MyWebView;
